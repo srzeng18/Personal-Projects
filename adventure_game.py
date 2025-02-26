0c142e59ebeb7c93e1257ba2ad867dff20aa0f71
srzeng8 <srzeng@umass.edu>
2025-02-25 21:35:33 -0500

    Intros and some conditions added

--- adventure_game.py
+++ adventure_game.py
@@ -1,1 +1,15 @@
 print("Welcome to my adventure game!")
+name = input("What is your name? ")
+print("Hello", name + "!")
+age = int(input("What is your age? "))
+
+if age >= 18:
+    print("You are old enough to play!")
+
+    wants_to_play = input("Do you want to play? ").lower()
+    if wants_to_play == "yes":
+        print("Let's play!")
+    else:
+        print("Thanks for checking out my game. Have a good day!")
+else:
+    print("You are not old enough to play. Sorry!")
